ed453b687c21ad6f3d294c67cdd9e8c508d6d816
TSA2003 <todorarnaudov03@schoolmath.eu>
2023-07-24 13:24:20 +0300

    Add entire project

---
+++ e-cBackend/src/eCommmerce/Backend/Application.java
@@ -0,0 +1,9 @@
+package eCommmerce.Backend;
+
+// Starting class
+public class Application {
+    // Starting point of app
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
