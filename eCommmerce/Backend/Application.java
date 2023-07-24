f2a3f85dc7757eee858e26900c238200230f00e8
TSA2003 <todorarnaudov03@schoolmath.eu>
2023-07-24 16:12:06 +0300

    Main

--- eCommmerce/Backend/Application.java
+++
@@ -1,9 +0,0 @@
-package eCommmerce.Backend;
-
-// Starting class
-public class Application {
-    // Starting point of app
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
